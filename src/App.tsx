d05ccebba009cea82f872ef664779d6a0e4b7cad
21130483 <21130483@st.hcmuaf.edu.vn>
2026-01-18 22:35:24 +0700

    trang tìm kiếm sản phẩm

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {router} from "./router/router";
@@ -6,8 +6,19 @@ import {Outlet, RouterProvider} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./component/Common/Header";
 import Footer from "./component/Common/Footer";
+import {useDispatch} from "react-redux";
+import {AppDispatch} from "./store/Store";
+import {fetchProducts} from "./store/productSlice";
+import {fetchCategories} from "./store/categorySlice";
 
 function App() {
+    const dispatch = useDispatch<AppDispatch>();
+
+    useEffect(() => {
+        // Nạp dữ liệu vào Redux ngay khi ứng dụng khởi chạy
+        dispatch(fetchProducts());
+        dispatch(fetchCategories()); // Rất quan trọng để hiện được Breadcrumb
+    }, [dispatch]);
   return (
 
       <div className="App">
